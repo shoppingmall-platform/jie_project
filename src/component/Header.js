542c549a9f715c51dd58d12ad31c9b21380ba1f6
mac <same2108@naver.com>
2024-08-09 15:11:04 +0900

    로그인페이지 추가

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,74 +1,102 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import logo from './../jle_logo.jpg';
-import { Button , Box, Grid,Typography } from '@mui/material';
+import { Button, Box, Grid,Typography, IconButton, Stack } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 // 메뉴 버튼 (mui Drawer)   user 아이콘 (로그인페이지 연결)   
-// 로고        장바구니     검색 
+// 로고(버튼 home page로 이동)      장바구니     검색 
 // 
-const menuItems = [
-    'Best', '[당일발송]', '머슬핏', 'New 5%', 'All', 'Outer', 'Top', 'Shirts', 'Bottom', 'ACC', 'Shoes','[MADE]',  '휴양룩'
-  ];
+
 const Header = () => {
-  return (
-    <Box sx={{flexDirection:'column'}}>
-        <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2, padding:'5px'}}>
-            <Button
-            sx={{color:'black'}}
-            startIcon={<MenuIcon />}>
-            </Button>
-            <Button
-            sx={{color:'black'}}
-            startIcon={<PersonOutlineOutlinedIcon />}>
-            </Button>
-            <Box>
-                <img src={logo} style={{ width: '80px', height: '40px'}}/>
+    const menuItems = [
+        'Best', '[당일발송]', '머슬핏', 'New 5%', 'All', 'Outer', 'Top', 'Shirts', 'Bottom', 'ACC', 'Shoes','[MADE]',  '휴양룩'
+      ];
+    const navigate = useNavigate()
+    const goToLogin=()=>{
+        navigate("/login");
+    };
+    const goToHome=()=>{
+        navigate("/");
+    };
+
+    return (
+        <Box sx={{flexDirection:'column'}}>
+            <Box 
+                sx={{
+                    display:'flex', 
+                    alignItems:'center',
+                    justifyContent:'space-between',
+                    padding:1.5,
+                    width:"100%"
+                    }}>
+                <Stack  direction='row' spacing={3}>
+                    <IconButton
+                    sx={{color:'black'}}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <IconButton
+                    sx={{color:'black'}}
+                    onClick={goToLogin}>
+                        <PersonOutlineOutlinedIcon />
+                    </IconButton>
+                </Stack>
+                <Box sx={{display:'flex' }}>
+                    <Button
+                        onClick={goToHome} // 클릭 시 동작
+                        sx={{ padding: 0, minWidth: 0 }} // 기본 패딩과 최소 너비 제거
+                        >
+                            <img src={logo} style={{ width: '80px', height: '40px'}}/>
+                    </Button>
+                </Box>
+                <Stack direction='row' spacing={3}>
+                    <IconButton
+                    sx={{color:'black'}}>
+                        <ShoppingBagOutlinedIcon />
+                    </IconButton>
+                    <IconButton
+                    sx={{color:'black'}}>
+                        <SearchIcon />
+                    </IconButton>
+                </Stack>
             </Box>
-            <Button
-            sx={{color:'black'}}
-            startIcon={<ShoppingBagOutlinedIcon />}>
-            </Button>
-            <Button
-            sx={{color:'black'}}
-            startIcon={<SearchIcon />}>
-            </Button>
-        </Box>
-        <Box sx={{ flexGrow: 1, padding: 1 }}>
-            <Grid container spacing={0.5}>
-                {menuItems.map((item, index) => (
-                <Grid item xs={3} key={index}> {/* 모바일에서도 4칸씩 배치 */}
-                    <Box
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        height: '80%',
-                        padding: 1,
-                        border: '1px solid #ddd',
-                        borderRadius: '4px',
-                        boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
-                        '&:hover': {
-                        backgroundColor: 'primary.light',
-                        }
-                    }}
-                    >
-                    <Typography
-                        variant="body1"
+            <Box sx={{ flexGrow: 1, padding: 1 }}>
+                <Grid container spacing={0.5}>
+                    {menuItems.map((item, index) => (
+                    <Grid item xs={3} key={index}> {/* 모바일에서도 4칸씩 배치 */}
+                        <Box
                         sx={{
-                        fontSize: '0.575rem', // 모바일에서의 폰트 크기 조정
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            height: '80%',
+                            padding: 1,
+                            border: '1px solid #ddd',
+                            borderRadius: '4px',
+                            boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+                            '&:hover': {
+                            backgroundColor: 'primary.light',
+                            }
                         }}
-                    >
-                        {item}
-                    </Typography>
-                    </Box>
+                        >
+                        <Typography
+                            variant="body1"
+                            sx={{
+                            fontSize: '0.575rem', // 모바일에서의 폰트 크기 조정
+                            }}
+                        >
+                            {item}
+                        </Typography>
+                        </Box>
+                    </Grid>
+                    ))}
                 </Grid>
-                ))}
-            </Grid>
+            </Box>
         </Box>
-    </Box>
-  )
+    )
 }
 
 export default Header
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,8 +1,96 @@
+import { Divider, Box, Button, Checkbox, Container, FormControlLabel,Grid, TextField, Typography } from '@mui/material';
 import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import AppleIcon from '@mui/icons-material/Apple';
+//아이디와 비밀번호를 입력해야 로그인버튼 활성화
+//유효성검사 필요
+//로그인 성공 시 페이지 이동
 
 const Login = () => {
+  //theme
   return (
-    <div>Login</div>
+    <Container maxWidth="xs">
+      <Typography align='center' >로그인</Typography>
+      <Divider />
+      <Box 
+        sx={{
+          display:'flex',
+          flexDirection:'column',
+          alignItems:'center',
+          marginTop:'20px'
+        }}>
+        <Box>
+          <TextField
+              size='small'
+              variant ='standard'
+              margin="dense"
+              required
+              fullWidth
+              id="email"
+              label="ID"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              size='small'
+              variant ='standard'
+              margin="dense"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control = {<Checkbox value="remember" color='primary'/>}
+              label={<span style={{ fontSize: '14px' }}>로그인 상태 유지</span>}
+            />
+            <Button 
+              fullWidth 
+              sx={{margin:'10px', backgroundColor:'black', color:'white', marginBottom:'20px'}}>
+                Login
+            </Button>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Button fullWidth> 회원가입 </Button>
+              </Grid>
+              <Grid item xs={6}>
+                <Button fullWidth> 아이디 찾기 </Button>
+              </Grid>
+              <Grid item xs={6}>
+                <Button fullWidth> 비밀번호 찾기 </Button>
+              </Grid>
+            </Grid>
+            <Box sx={{marginTop:'30px'}}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Button 
+                    fullWidth
+                    sx={{color:'black'}}
+                    startIcon={<MenuIcon />}
+                    > 네이버로 로그인 </Button>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button 
+                    fullWidth
+                    sx={{color:'black'}}
+                    startIcon={<MenuIcon />}> 카카오로 로그인 </Button>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button 
+                    fullWidth
+                    sx={{color:'black'}}
+                    startIcon={<AppleIcon />}> Apple로 로그인 </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+      </Box>
+    </Container>
+
   )
 }
 
