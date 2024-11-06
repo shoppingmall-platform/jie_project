f37fca695ce3739ca1d56f627abab3f68e1b70bf
mac <same2108@naver.com>
2024-11-06 18:47:44 +0900

    s3 이미지 처리

--- src/admin/component/Editor.js
+++ src/admin/component/Editor.js
@@ -1,74 +1,124 @@
 import { Button } from '@mui/material';
-import {React, useState, useMemo, useRef} from 'react'
+import React, { useState, useMemo, useRef } from 'react';
 import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import { ImageActions } from '@xeger/quill-image-actions';
-import { ImageFormats }from '@xeger/quill-image-formats';
+import ImageResize from 'quill-image-resize';
+import AWS from 'aws-sdk';
 
-Quill.register('modules/imageActions', ImageActions);
-Quill.register('modules/imageFormats', ImageFormats);
 
+
+if (!Quill.imports['modules/imageResize']) {
+    Quill.register('modules/imageResize', ImageResize);
+  }
+
+AWS.config.update({
+    region: process.env.REACT_APP_awsRegion,
+    credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId:process.env.REACT_APP_awsIdentityPoolId,
+    }),
+});
+
+const s3 = new AWS.S3();
 
 const EditorBox = () => {
     const [value, setValue] = useState('');
     const quillRef = useRef();
 
-    const handleSubmit = () => {
-        console.log('상세설명 저장')
-    }
+    const handleSubmit = async () => {
+        const editorContent = quillRef.current.getEditor().root.innerHTML;
+        const images = editorContent.match(/(<img[^>]*src\s*=\s*[\"']?([^>\"']+)[\"']?[^>]*>)/g) || [];
+        const updatedContent = await handleImageUpload(images, editorContent);
+        console.log("서버로 보낼 HTML:", updatedContent); //나중에 여기다가 서버로 보내기, 함수는 따로 작성
+    };
+
+    const handleImageUpload = async (images, editorContent) => {
+        for (let image of images) {
+            const base64Src = image.match(/src="([^"]+)"/)[1]; 
+            const blob = base64ToBlob(base64Src);
+            const imageUrl = await uploadImage(blob);
+            editorContent = editorContent.replace(base64Src, imageUrl);
+        }
+        return editorContent;
+    };
+    const base64ToBlob = (base64) => {
+        const byteCharacters = atob(base64.split(',')[1]);
+        const byteArrays = [];
+        for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+            const slice = byteCharacters.slice(offset, offset + 512);
+            const byteNumbers = new Array(slice.length);
+            for (let i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+            byteArrays.push(new Uint8Array(byteNumbers));
+        }
+        return new Blob(byteArrays, { type: 'image/jpeg' }); 
+    };
+
+    const uploadImage = async (blob) => {
+        try {
+            const params = {
+                Bucket: process.env.REACT_APP_bucketName,
+                Key: `uploads/${Date.now()}`,  
+                Body: blob,                   
+                ContentType: blob.type       
+            };
+            const { Location } = await s3.upload(params).promise();
+            return Location; 
+        } catch (err) {
+            console.error('S3 Upload Error:', err);
+            throw new Error('이미지 업로드 중 오류가 발생했습니다.');
+        }
+    };
+
     const formats = [
-        "header",
-        "bold",
-        "italic",
-        "underline",
-        "strike",
-        "blockquote",
-        "list",
-        "bullet",
-        "indent",
-        "link",
-        "image",
-        "video",
-        'font',
-        'float',
-        'align',
-        'height',
-        'width'
-      ];
-    const modules = useMemo(()=> {
+        "header", "bold", "italic", "underline", "strike", "blockquote", "list",
+        "bullet", "indent", "link", "image", "video", 'font', 'float', 'align',
+        'height', 'width','color', 'background'
+    ];
+
+    const modules = useMemo(() => {
         return {
-            imageActions: {},
-            imageFormats: {},
             toolbar: {
                 container: [
                     [{ 'font': [] }],
-                    [{ header: [1, 2, false] }],
+                    [{ header: [1,2,3,4,5,6,false] }],
                     ['bold', 'italic', 'underline', 'strike', 'blockquote'],
                     [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
                     ['link', 'image', 'video'],
-                    [{ align: [] }, { color: [] }, { background: [] }], 
+                    [{ align: [] }, { color: [] }, { background: [] }],
                     ['clean'],
-                ],
-
+                ]
             },
-            
+            imageResize: {
+                displayStyles: {
+                    backgroundColor: 'black',
+                    border: 'none',
+                    color: 'white'
+                },
+                modules: [ 'Resize', 'DisplaySize', 'Toolbar' ]
+            }
         }
     }, []);
 
+    const onChangeValue = (e) => {
+        console.log(e);
+        setValue(e);
+    };
 
-    return(
+    return (
         <div>
-            <ReactQuill 
-            value={value} 
-            theme="snow"
-            ref={quillRef}
-            onChange={setValue}
-            formats={formats}
-            modules={modules}
-            style={{height:400 , overflowY:'auto'}} />
+            <ReactQuill
+                value={value}
+                theme="snow"
+                ref={quillRef}
+                onChange={onChangeValue}
+                formats={formats}
+                modules={modules}
+                style={{ height: 400, overflowY: 'auto' }}
+            />
             <Button onClick={handleSubmit}>저장</Button>
         </div>
-    )
-}
+    );
+};
 
-export default EditorBox
+export default EditorBox;
--- src/admin/component/UploadFile.js
+++ src/admin/component/UploadFile.js
@@ -1,86 +1,122 @@
-import { Alert, Button , IconButton} from '@mui/material';
-import React, { useState,useRef } from 'react';
+import { Alert, Button, IconButton } from '@mui/material';
+import React, { useState, useRef } from 'react';
+import AWS from 'aws-sdk';
 
+AWS.config.update({
+    region: process.env.REACT_APP_awsRegion,
+    credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId:process.env.REACT_APP_awsIdentityPoolId,
+    }),
+});
 
-const UploadFile = ({maxImages}) => {
+const s3 = new AWS.S3();
+
+const UploadFile = ({ maxImages }) => {
     const [images, setImages] = useState([]);
-    const [error, setError] = useState(''); 
+    const [error, setError] = useState('');
     const inputRef = useRef(null);
 
     const handleImageChange = (e) => {
         const files = Array.from(e.target.files);
         const currentImageCount = images.length;
-        // const maxImages = 10;
-        
-        // 파일이 maximages 이상일 경우 경고 메시지를 표시
+
         if (currentImageCount + files.length > maxImages) {
             setError(`최대 ${maxImages}개의 이미지만 업로드할 수 있습니다.`);
-            e.target.value = ''; // 입력 초기화
+            e.target.value = '';
             return;
         } else {
-            setError(''); // 경고 메시지 초기화
+            setError('');
         }
 
-        const validImages = files.filter(file => file.type.startsWith('image/')).slice(0, maxImages - currentImageCount);
+        const validImages = files.filter((file) => file.type.startsWith('image/')).slice(0, maxImages - currentImageCount);
 
-        validImages.forEach(file => {
+        validImages.forEach((file) => {
             const reader = new FileReader();
             reader.onloadend = () => {
-                setImages(prevImages => [...prevImages, reader.result]);
+                setImages((prevImages) => [...prevImages, { file, preview: reader.result }]);
             };
-            reader.readAsDataURL(file); // Convert the image file to base64
+            reader.readAsDataURL(file);
         });
 
-        e.target.value = ''; // Clear the input
+        e.target.value = '';
     };
 
     const handleRemoveImage = (index) => {
-        setImages(prevImages => prevImages.filter((_, i) => i !== index));
+        setImages((prevImages) => prevImages.filter((_, i) => i !== index));
     };
+
     const handleButtonClick = () => {
         if (inputRef.current) {
             inputRef.current.click();
         }
     };
 
+    const saveEventHandler = async () => {
+        try {
+            const uploadedUrls = await Promise.all(
+                images.map(async ({ file }) => {
+                    const params = {
+                        Bucket: process.env.REACT_APP_bucketName,
+                        Key: `uploads/${Date.now()}-${file.name}`,
+                        Body: file,
+                        ContentType: file.type,
+                        
+                    };
+
+                    const { Location } = await s3.upload(params).promise();
+                    return Location;
+                })
+            );
+            
+            console.log('Uploaded URLs:', uploadedUrls);
+        } catch (err) {
+            console.error('S3 Upload Error:', err);
+            setError('이미지 업로드 중 오류가 발생했습니다.');
+        }
+    };
+
     return (
         <div>
-            <input 
-                type="file" 
-                accept="image/*" 
+            <input
+                type="file"
+                accept="image/*"
                 multiple
-                onChange={handleImageChange} 
+                onChange={handleImageChange}
                 ref={inputRef}
-                style={{display:'none'}}
+                style={{ display: 'none' }}
             />
-            <Button 
-            variant="outlined" 
-            size='small'
-            color="primary" 
-            onClick={handleButtonClick}
-            sx={{margin:1}}>
+            <Button
+                variant="outlined"
+                size="small"
+                color="primary"
+                onClick={handleButtonClick}
+                sx={{ margin: 1 }}
+            >
                 파일 선택
             </Button>
-            {/* 경고 메시지 표시 */}
             {error && <Alert severity="error" variant="outlined">{error}</Alert>}
-            
             <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%' }}>
-                {images.map((image, index) => (
+                {images.map(({ preview }, index) => (
                     <div key={index} style={{ position: 'relative', flexDirection: 'column', margin: '5px' }}>
-                        <img 
-                            src={image} 
-                            alt={`preview-${index}`} 
-                            style={{ width: '100px', height: '100px', objectFit: 'cover' }} 
+                        <img
+                            src={preview}
+                            alt={`preview-${index}`}
+                            style={{ width: '100px', height: '100px', objectFit: 'cover' }}
                         />
-                        <IconButton onClick={() => handleRemoveImage(index)}  sx={{position:'absolute', top:'0px', right:'6px'}} size="small">
+                        <IconButton
+                            onClick={() => handleRemoveImage(index)}
+                            sx={{ position: 'absolute', top: '0px', right: '6px' }}
+                            size="small"
+                        >
                             &times;
                         </IconButton>
-
                     </div>
                 ))}
             </div>
+            <Button onClick={saveEventHandler} sx={{ marginLeft: '80%' }}>저장</Button>
         </div>
     );
 };
 
 export default UploadFile;
+
